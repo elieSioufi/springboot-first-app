b5c9dda9246863a1ceac3cade947e426073d4b86
CORP-AD\w190959 <elie.sioufi@worldline.com>
2024-06-11 11:40:07 +0200

    removing a folder and the commented code

--- src/main/java/com/testcase/controller/WelcomeController.java
+++ src/main/java/com/testcase/controller/WelcomeController.java
@@ -1,4 +1,4 @@
-package com.testcase;
+package com.testcase.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,10 +17,3 @@ public class WelcomeController {
 
 
 
-//@RestController
-//public class WelcomeController {
-//  @GetMapping("/welcome")
-//public String welcome(){
-//          return"Success" ;
-//}
-//}
